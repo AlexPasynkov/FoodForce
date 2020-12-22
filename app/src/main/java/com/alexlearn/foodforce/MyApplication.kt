042ce75f9f860e2643ffd80569696a1b4a60be04
AlexPasynkov <54889913+AlexPasynkov@users.noreply.github.com>
2020-12-22 16:06:26 +0100

    Initial commit

---
+++ app/src/main/java/com/alexlearn/foodforce/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.alexlearn.foodforce
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+}
---
+++ app/src/main/java/com/alexlearn/foodforce/data/RemoteDataSource.kt
@@ -0,0 +1,16 @@
+package com.alexlearn.foodforce.data
+
+import com.alexlearn.foodforce.data.network.FoodRecipesApi
+import com.alexlearn.foodforce.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+
+}
