dca96ec92d3bf6c7af801b45f78e87fbfd436f1c
AlexPasynkov <54889913+AlexPasynkov@users.noreply.github.com>
2020-12-29 12:56:55 +0100

    Updated details fragment

--- app/src/main/java/com/alexlearn/foodforce/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/alexlearn/foodforce/ui/fragments/instructions/InstructionsFragment.kt
@@ -1,4 +1,4 @@
-package com.alexlearn.foodforce.ui.fragments
+package com.alexlearn.foodforce.ui.fragments.instructions
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.alexlearn.foodforce.R
 
-class OverviewFragment : Fragment() {
+
+class InstructionsFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        return inflater.inflate(R.layout.fragment_instructions, container, false)
     }
 
 
